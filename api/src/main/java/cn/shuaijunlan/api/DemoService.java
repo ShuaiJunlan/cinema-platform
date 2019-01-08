52b668236e49d25cf1905971cdd9f0b2acd4bde6
Junlan Shuai <shuaijunlan@gmail.com>
2019-01-08 19:06:53 +0800

    update

--- api/src/main/java/cn/shuaijunlan/api/DemoService.java
+++
@@ -1,9 +0,0 @@
-package cn.shuaijunlan.api;
-
-/**
- * @author Shuai Junlan[shuaijunlan@gmail.com].
- * @since Created in 7:27 PM 12/28/18.
- */
-public interface DemoService {
-    String sayHello(String name);
-}
