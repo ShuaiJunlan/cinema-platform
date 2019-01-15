96fede9bcf337e1c91ff17e7a76f58f3f89d94ed
Junlan Shuai <shuaijunlan@gmail.com>
2019-01-15 13:47:02 +0800

    update

--- orders-manage/order-services/src/main/java/cn/shuaijunlan/orderservices/OrderServicesApplication.java
+++ orders-manage/order-services/src/main/java/cn/shuaijunlan/orderservices/OrderServicesApplication.java
@@ -1,5 +1,6 @@
 package cn.shuaijunlan.orderservices;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shuaijunlan
  */
 @SpringBootApplication
+@EnableDubbo
 public class OrderServicesApplication {
 
     public static void main(String[] args) {
