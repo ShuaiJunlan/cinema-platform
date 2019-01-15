e2c6ff4f85959b9e6345507c4871447c3fb8625b
Junlan Shuai <shuaijunlan@gmail.com>
2019-01-15 13:30:49 +0800

    add order manage module

---
+++ orders-manage/order-services/src/main/java/cn/shuaijunlan/orderservices/OrderServicesApplication.java
@@ -0,0 +1,17 @@
+package cn.shuaijunlan.orderservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuaijunlan
+ */
+@SpringBootApplication
+public class OrderServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServicesApplication.class, args);
+    }
+
+}
+
